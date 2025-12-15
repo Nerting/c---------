b606128df29a5b86befc5645750e89768025e8ca
Denis <denelets1@gmail.com>
2025-12-15 03:25:53 +0300

    first commit

---
+++ VisualStudioCode/SimpleCSharpConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿/*4 ЗАДАНИМЕ МОЕ 
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+          var numbers = new[] { 5, 1, 9, 3, 7 };
+        var sorted = numbers.OrderByDescending(n => n);
+        Console.WriteLine("По убыванию: " + string.Join(", ", sorted));
+    }
+}
+*/ 
+/* 13 ЗАДАНИЕ 
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var numbers = new[] { 10, 30, 20, 50, 40 };
+        var min = numbers.Min();
+        Console.WriteLine("Минимум: " + min);
+    }
+}
+*/
